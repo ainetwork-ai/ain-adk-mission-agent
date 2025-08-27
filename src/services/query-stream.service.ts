3544fd601bc7fca70cacdd78e5086891e615f14b
Yoojin Ko <db232100@gmail.com>
2025-08-27 22:00:00 +0900

    fix: camel

--- src/services/query-stream.service.ts
+++ src/services/query-stream.service.ts
@@ -378,7 +378,7 @@ ${JSON.stringify(intentResult.result)}
 						event: "mission_reward",
 						data: {
 							reward: answerResult.reward,
-							total_reward: answerResult.total_reward,
+							total_reward: answerResult.totalPoint,
 						},
 					};
 
