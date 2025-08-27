811d23e95ce0f6c9c81161a5ce17637300999019
Yoojin Ko <db232100@gmail.com>
2025-08-27 17:58:08 +0900

    feat: mission

---
+++ src/services/mission.service.ts
@@ -0,0 +1,50 @@
+const SERVER_URL = process.env.SERVER_URL;
+
+export async function getMission(userId: string, token: string) {
+	const response = await fetch(
+		`${SERVER_URL}/api/mission/random?addr=${userId}`,
+		{
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		},
+	);
+
+	return response.json();
+}
+
+export async function submitAnswer(
+	userId: string,
+	answer: string,
+	token: string,
+) {
+	const body = {
+		answer,
+		addr: userId,
+	};
+	const response = await fetch(`${SERVER_URL}/api/mission/answer`, {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+
+	return response.json();
+}
+
+export async function getRemainMissionCount(userId: string, token: string) {
+	const response = await fetch(
+		`${SERVER_URL}/api/mission/remain?addr=${userId}`,
+		{
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		},
+	);
+
+	return response.json();
+}
